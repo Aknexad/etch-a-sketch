b3f8e49356bc5c899fa438469b531f4154a83d33
Aknexad <aknejad@outlook.com>
2022-06-26 14:56:37 +0430

    add draw function

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function boxs() {
-  let container = document.querySelector('.container');
+  let container = document.querySelector('.gridArya');
   let addDiv = document.createElement('div');
   container.appendChild(addDiv);
   addDiv.classList.add('empty');
 }
 
+const Draw = () => {
+  let alldiv = document.querySelectorAll('.empty');
+  alldiv.forEach((element) => {
+    element.addEventListener('mouseenter', () => {
+      element.classList.add('fill');
+    });
+  });
+};
+
 for (let i = 0; i < 256; i++) {
   boxs();
 }
 
-let alldiv = document.querySelectorAll('.empty');
-alldiv.forEach((element) => {
-  element.addEventListener('mouseenter', () => {
-    element.classList.add('fill');
-  });
-});
+Draw();
