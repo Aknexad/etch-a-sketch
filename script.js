8607420f54472cdd2d82ccf559b816718f02a1b0
Aknexad <aknejad@outlook.com>
2022-06-27 11:22:59 +0430

    add gridArea and game function

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function boxs() {
-  let container = document.querySelector('.gridArya');
-  let addDiv = document.createElement('div');
-  container.appendChild(addDiv);
-  addDiv.classList.add('empty');
+function gridArea(sizeValue) {
+  // clean grid area
+  let clearGrid = document.querySelectorAll('div.empty');
+  clearGrid.forEach((element) => {
+    element.remove();
+  });
+
+  // Change css style grid
+  let columRowSize = document.querySelector('.gridArea');
+  columRowSize.style.gridTemplateColumns = `repeat(${sizeValue}, 1fr)`;
+  columRowSize.style.gridTemplateRows = `repeat(${sizeValue}, 1fr)`;
+
+  // create gride
+  for (let i = 0; i < sizeValue * sizeValue; i++) {
+    let container = document.querySelector('.gridArea');
+    let addDiv = document.createElement('div');
+    container.appendChild(addDiv);
+    addDiv.classList.add('empty');
+  }
 }
 
-const Draw = () => {
+const draw = () => {
   let alldiv = document.querySelectorAll('.empty');
   alldiv.forEach((element) => {
     element.addEventListener('mouseenter', () => {
@@ -14,8 +28,13 @@ const Draw = () => {
   });
 };
 
-for (let i = 0; i < 256; i++) {
-  boxs();
-}
+const game = () => {
+  let selectInput = document.querySelector('#gtidSize');
+  selectInput.addEventListener('click', (e) => {
+    let selectValue = e.target.value;
+    gridArea(selectValue);
+    draw();
+  });
+};
 
-Draw();
+game();
